816595d6a52912a97e5c77d62f397d10257ff7ea
Rohit Kumar Paramanandam (MS/EAC4-AE) <rohitkumar.paramanandam@bosch.com>
2025-03-11 19:08:47 +0530

    Removed pre-build library and bin files

--- firmware/app3.1/coines_bridge/update_coines_bridge_ram_fw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script updates APP3.0 board with Coines Bridge firmware
-if ! [ -f "../../../tools/app_switch/app_switch" ]; then
-    make -C ../../../tools/app_switch
-fi
-
-../../../tools/app_switch/app_switch usb_dfu_bl
-dfu-util --device -,108c:ab39 -a RAM -D coines_bridge_ram_firmware.bin -R
